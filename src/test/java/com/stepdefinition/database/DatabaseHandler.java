5b84ce1ed1f47e0967b1ff10f91bb6827c1039ee
unknown <supunwimukthi@gmail.com>
2023-07-08 15:11:49 +0530

    Remove DB Credentials and add it to a property file

--- src/test/java/com/stepdefinition/database/DatabaseHandler.java
+++ src/test/java/com/stepdefinition/database/DatabaseHandler.java
@@ -7,20 +7,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.util.ConfigurationManager;
+
 public class DatabaseHandler {
 
-	private static final String JDBC_URL = "jdbc:mysql://localhost:3306/cucumber";
-	private static final String JDBC_UN = "root";
-	private static final String JDBC_PW = "123456";
 	public static final String INSERT_INSTRUMENT = "INSERT INTO instrument_details (id, name, isin, unit_price) VALUES (?, ?, ?, ?)";
 	public static final String INSERT_POSITION = "INSERT INTO position_details (id, instrument_id, qty) VALUES (?, ?, ?)";
 	public static final String INSERT_POSITION_REPORT = "INSERT INTO position_report (id, position_id, isin, qty, total_price) SELECT p.id, p.id, i.isin, p.qty, p.qty * i.unit_price FROM position_details p JOIN instrument_details i on p.instrument_id = i.id";
 	public static final String SELECT_POSITION_REPORT_COUNT = "SELECT COUNT(*) FROM position_report WHERE total_price IS NOT NULL";
 	public static final String SELECT_POSITION_REPORT = "SELECT id, position_id, isin, qty, total_price FROM position_report";
+	private static final String JDBC_URL = "jdbc.url";
+	private static final String JDBC_UN= "jdbc.username";
+	private static final String JDBC_PW = "jdbc.password";
 	
 	private static Connection getConnection() throws SQLException {
 		
-		return DriverManager.getConnection(JDBC_URL, JDBC_UN, JDBC_PW);
+		ConfigurationManager dbConfiguration = ConfigurationManager.getInstance();
+		
+		String jdbc_url = dbConfiguration.getProperty(JDBC_URL);
+		String jdbc_username = dbConfiguration.getProperty(JDBC_UN);
+		String jdbc_password = dbConfiguration.getProperty(JDBC_PW);
+		
+		return DriverManager.getConnection(jdbc_url, jdbc_username, jdbc_password);
 	}
 	
 	public static PreparedStatement getPreparedStatement(String sqlQuery) throws SQLException {
